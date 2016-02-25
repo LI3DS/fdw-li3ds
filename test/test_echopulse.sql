41446476ec1f52491c9094ce526df591096dbc5c
Vincent Picavet <vincent.picavet@oslandia.com>
2016-02-25 13:52:27 +0100

    Add a SQL test script

---
+++ test/test_echopulse.sql
@@ -0,0 +1,18 @@
+drop extension multicorn cascade;
+create extension multicorn;
+
+create server echopulse foreign data wrapper multicorn
+    options (
+        wrapper 'fdwlidar.echopulse.EchoPulse'
+        , raw 'data/pulse-float32-phi/43724.bin'
+        , theta 'data/pulse-float32-theta/43724.bin'
+        , time 'data/pulse-linear-time/43724.txt'
+    );
+
+create foreign table myechopulse (
+    r float
+    , theta float
+    , time float 
+) server echopulse;
+
+select * from myechopulse;
